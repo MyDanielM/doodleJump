5d05e51583dbbb44b022f54aa11941c044f269a4
myDanielM <godinseccoming@gmail.com>
2020-12-03 08:11:01 +0300

    init

---
+++ Assets/Scripts/PlatformGenerate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerate : MonoBehaviour
+{
+    public GameObject platform;
+    private Vector3 SpawnPosition = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPosition.y = 0;
+        for ( int i =0; i<10; i++)
+        {
+            SpawnPosition.x = Random.Range(-1.7f, 1.7f);
+            SpawnPosition.y += Random.Range(1.2f, 1.5f);
+            Instantiate(platform, SpawnPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float forceJump = 10f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnLeftButtonDown()
+    {
+        rb.velocity = new Vector2(-1f * speed, rb.velocity.y);
+    }
+
+    public void OnRightButtonDown()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+    public void OnButtonUp()
+    {
+        rb.velocity = new Vector2(0f, rb.velocity.y);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            if (collision.relativeVelocity.y >= 0)
+            {
+                rb.velocity = Vector2.up * forceJump;
+            }
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene("Defeat");
+        }
+
+    }
+
+}
